b1d2a94e180f6ee533eb3bd6e7db83871f0192ba
Junhyung Kim <48814228+junhyung9985@users.noreply.github.com>
2023-11-06 01:44:31 +0900

    [Bronze I] Title: 바닥수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30456. 바닥수/바닥수.cc
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+// Default
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+// PBDS
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    ll N, L;
+    cin >> N >> L;
+
+    for(int i =0; i<L-1; ++i)
+        cout << 1;
+    cout<<N;
+
+    return 0;
+} // Ad_hoc
