d21e46d22ede16d5194460e686a3de09ce45c266
Junhyung Kim <48814228+junhyung9985@users.noreply.github.com>
2023-11-08 17:26:16 +0900

    [Bronze V] Title: A+B - 5, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a, b;
+    while(true){
+        cin >> a >> b;
+        if(a == 0 && b == 0) return 0;
+        cout << a+b<<"\n";
+    }
+}
