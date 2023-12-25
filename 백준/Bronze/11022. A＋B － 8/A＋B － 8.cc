2b991d3cd8bc1c26f5d74320825fed5035d39594
Junhyung Kim <48814228+junhyung9985@users.noreply.github.com>
2023-12-25 17:15:54 +0900

    [Bronze V] Title: A+B - 8, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    for(int i =1 ;i<=T; ++i){
+        int a,b;
+        cin >> a >> b;
+        cout <<"Case #"<<i<<": "<<a<<" + "<<b<<" = "<<a+b<<"\n";
+    }
+    return 0;
+}
