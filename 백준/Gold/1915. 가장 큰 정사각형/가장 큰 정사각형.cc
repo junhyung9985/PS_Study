3a32d696c6bd18571f9a93ee9786b77e385b7bbf
Junhyung Kim <48814228+junhyung9985@users.noreply.github.com>
2024-01-20 17:14:08 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 36 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.cc
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    ll N, M;
+    cin >> M >> N;
+    
+    vector<vector<ll>> v(M, vector<ll>(N));
+    vector<vector<ll>> dp(M, vector<ll>(N));
+    
+    for(int i =0; i<M; ++i){
+        for(int j =0; j<N; ++j){
+            char ch;
+            cin >> ch;
+            v[i][j] = ch - '0';
+        }
+    }
+    
+    ll maxx = 0;
+    for(int i =0; i<M; ++i){
+        for(int j =0; j<N; ++j){
+            if(v[i][j]){
+                dp[i][j] = 1;
+                if(i > 0 && j > 0) {
+                    if(v[i-1][j] && v[i][j-1] && v[i-1][j-1]){
+                        ll smallest = dp[i-1][j];
+                        smallest = min(smallest, dp[i-1][j]);
+                        smallest = min(smallest, dp[i][j-1]);
+                        smallest = min(smallest, dp[i-1][j-1]);
+                        dp[i][j] += smallest;
+                    }
+                }
+                maxx = max(maxx, dp[i][j]);
+            }
+            //cout << dp[i][j] <<" ";
+        }
+        //cout<<"\n";
+    }
+    
+    cout << maxx * maxx;
+
+    return 0;
+} // 2D Prefix Sum
