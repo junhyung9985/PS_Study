b88c7fa9d99bb8edff7f9482ac37aef3ad600528
Junhyung Kim <48814228+junhyung9985@users.noreply.github.com>
2024-01-23 16:33:50 +0900

    [Platinum V] Title: 도로포장, Time: 116 ms, Memory: 13356 KB -BaekjoonHub

--- 백준/Platinum/1162. 도로포장/도로포장.cc
+++ 백준/Platinum/1162. 도로포장/도로포장.cc
@@ -1,60 +1,102 @@
 #include <bits/stdc++.h>
 #define ll long long
-
 using namespace std;
 
-int main()
-{
+bool sort_cmp(tuple<ll,ll,ll> a, tuple<ll,ll,ll> b){
+    if(get<1>(a) == get<1>(b)){
+        return get<2>(a) < get<2>(b);
+    }
+    return get<1>(a) < get<1>(b);
+}
+
+class Dijkstra_2d{
+    public :
+        ll N, M;
+        vector<vector<tuple<ll,ll,ll>>> E, E2;
+
+        Dijkstra_2d(ll num_nodes, ll capacity){
+            this-> N = num_nodes;
+            this-> M = capacity;
+            this->E.resize(N+1, vector<tuple<ll,ll,ll>>());
+            //this->E2.resize(N+1, vector<tuple<ll,ll,ll>>());
+        };
+
+        void add_edge(ll from, ll to, ll weight, ll cost){
+            this->E[from].push_back({to,weight, cost});
+            //this->E2[to].push_back({from,weight,cost});
+            return;
+        }
+
+        void sort_edges(){
+            for(int i = 1; i<=N; ++i)
+                sort(E[i].begin(), E[i].end(), sort_cmp);
+        }
+
+        vector<vector<ll>> run(ll src, ll dst = -1){
+
+            priority_queue<tuple<ll,ll,ll>> pq;
+            vector<vector<ll>> dist(M+1, vector<ll>(N+1, -1)); // array of shortest distance from src, -1 if unreachable
+            dist[0][src] = 0;
+            for(int i =1; i<=M; ++i) dist[i][src] = 0;
+            pq.push({0, src, 0});
+            
+            while(!pq.empty()){
+                ll d, curr, cst;
+                tie(d,curr,cst) = pq.top();
+                pq.pop();
+                d *= -1;
+                if(dist[cst][curr] < d) continue;
+                if(curr == dst) break;
+                for(auto itr : this->E[curr]){
+                    ll next,cost,cst2;
+                    tie(next,cost,cst2) = itr;
+                    if(cst+cst2 > this->M) continue;
+                    if(dist[cst+cst2][next] == -1 || dist[cst+cst2][next] > d+cost){
+                        dist[cst+cst2][next] = d+cost;
+                        for(int j = cst+cst2; j<=this->M; ++j){
+                            if(dist[j][next] == -1) dist[j][next] = dist[cst+cst2][next];
+                            else dist[j][next] = min(dist[j][next], dist[cst+cst2][next]);
+                        }
+                        pq.push({-d-cost, next, cst+cst2});
+                    }
+                }
+            }
+            return dist;
+        }; // return all the shortest distance from src to all nodes
+
+        ll run_dst(ll src, ll dst){
+            return run(src)[M][dst];
+        }; // return shortest distance from src to dst
+
+        void run_backtrack(ll src, ll dst){
+            return;
+        }; // I don't know how... also, I haven't seen any 2D-dijkstra backtracking problem yet.
+        // I will update about this after I've seen any problem like this.
+
+}; // Costive Dijkstra
+
+int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     
-    ll N, M, K;
-    cin >> N >> M >> K;
+    ll n, m, k;
+    cin >> n >> m >> k;
     
-    vector<vector<ll>> dist(K+1, vector<ll>(N+1, 1'000'000'000'000));
-    vector<vector<pair<ll,ll>>> E(N+1);
-    for(int i =0; i<M; ++i){
-        ll a, b, c;
-        cin >> a >> b >> c;
-        E[a].push_back({c,b});
-        E[b].push_back({c,a});
-    }
-    for(int i =1; i<=N ;++i){
-        sort(E[i].begin(), E[i].end());
+    Dijkstra_2d dijkstra(n,k);
+    
+    for(int i =0; i<m ; ++i){
+        ll a,b,d;
+        cin >> a >> b >> d;
+        dijkstra.add_edge(a,b,d,0);
+        dijkstra.add_edge(a,b,0,1);
+        dijkstra.add_edge(b,a,d,0);
+        dijkstra.add_edge(b,a,0,1);
     }
     
-    for(int i =0; i<=K; ++i)
-        dist[i][1] = 0;
-        
-    priority_queue<tuple<ll,ll,ll>> pq;
-    pq.push({0,1,0});
-    while(!pq.empty()){
-        ll d, node, cost;
-        tie(d, node, cost) = pq.top();
-        pq.pop();
-        d *= -1;
-        if(dist[cost][node] < d) continue;
-        for(auto itr : E[node]){
-            ll t, n;
-            tie(t,n) = itr;
-            if(cost < K){
-                if(dist[cost+1][n] > d){
-                    dist[cost+1][n] = d;
-                    for(int l = cost+2; l<=K; ++l) dist[l][n] = min(dist[l][n], dist[cost+1][n]);
-                    pq.push({-d, n, cost+1});
-                }
-            }
-            if(dist[cost][n] > d+t){
-                dist[cost][n] = d+t;
-                for(int l = cost+1; l<=K; ++l) dist[l][n] = min(dist[l][n], dist[cost][n]);
-                pq.push({-d-t, n, cost});
-            }
-        }
-    }
-    ll ans = dist[0][N];
-    for(int i = 1; i<=K; ++i)
-        ans = min(ans ,dist[i][N]);
-    cout << ans;
+    
+    ll dist = dijkstra.run_dst(1,n);
+    cout << dist;
+
+
     return 0;
-} // Dijkstra + DP 
-// Cost가 존재하는 Dijkstra 문제
+} // Class 확인
