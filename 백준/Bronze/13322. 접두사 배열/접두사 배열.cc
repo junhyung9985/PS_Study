bb85403570fd86c0fcad9ecc31fd537901aea66e
Junhyung Kim <48814228+junhyung9985@users.noreply.github.com>
2024-01-26 16:57:13 +0900

    [Bronze I] Title: 접두사 배열, Time: 12 ms, Memory: 2300 KB -BaekjoonHub

---
+++ 백준/Bronze/13322. 접두사 배열/접두사 배열.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    string s;
+    cin >> s;
+    for(int i =0; i<s.size(); ++i)
+        cout << i <<"\n";
+    
+    return 0;
+} // String, Ad_Hoc(???)
