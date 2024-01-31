3e55d5a4f6d558214a6c8975f5ec76e73c973ae6
Junhyung Kim <48814228+junhyung9985@users.noreply.github.com>
2024-02-01 02:39:37 +0900

    [Platinum IV] Title: 구간 합 구하기 2, Time: 224 ms, Memory: 87980 KB -BaekjoonHub

--- 백준/Platinum/10999. 구간 합 구하기 2/구간 합 구하기 2.cc
+++ 백준/Platinum/10999. 구간 합 구하기 2/구간 합 구하기 2.cc
@@ -1,82 +1,105 @@
 #include<bits/stdc++.h>
 #define ll long long
+
 using namespace std;
-// Default
+template <typename T>
+class LazySegmentTree{
+    public: 
+        ll N;
+        vector<T> tr;
+        vector<T> lazy;
+        vector<T> arr;
 
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
-// PBDS
-vector<ll> arr;
-vector<ll> tr;
-vector<ll> lazy;
+        void push(ll n, ll s, ll e){
+            tr[n] += (e-s+1) * lazy[n];
+            if(s != e){
+                lazy[2*n] += lazy[n];
+                lazy[2*n+1] += lazy[n];
+            }
+            lazy[n] = 0;
+            return;
+        }// Needs to be implemented by user.
+        // Define this based on the type of the query
 
-void push(ll n, ll s, ll e){
-    if(lazy[n] != 0){
-        tr[n] += (e-s+1)*lazy[n];
-        if(s != e){
-            lazy[n* 2] += lazy[n];
-            lazy[n * 2+1] += lazy[n];
+        T func(T a, T b){
+            return a+b;
+        }// Needs to be implemented by user.
+        // Define this based on the type of the query
+
+        T init(ll n, ll s, ll e){
+            if(s == e) return this->tr[n] = this->arr[s];
+            ll mid = (s+e)/2;
+            return this->tr[n] = func(init(2*n, s, mid), init(2*n+1, mid+1, e));
         }
-        lazy[n] = 0;
-    }
-}
 
-void update_range(ll n, ll s, ll e, ll l, ll r, ll val){
-    push(n, s, e);
-    if(e < l || r < s) return;
-    if(s >= l && e <= r){
-        lazy[n] += val;
-        push(n,s,e);
-        return;
-    }
-    ll mid = (s+e)/2;
-    update_range(n*2 , s, mid, l, r, val);
-    update_range(n*2+1, mid+1, e, l, r, val);
-    tr[n] = tr[n*2]+tr[n*2+1];
-}
+        LazySegmentTree(vector<T> v){
+            this->N = v.size();
+            this->arr.resize(this->N);
+            this->tr.resize(4 * (this->N));
+            this->lazy.resize(4 * (this->N));
+            for(int i =0; i<N; ++i)
+                this->arr[i] = v[i];
 
-ll query(ll n, ll s, ll e, ll l, ll r){
-    push(n, s, e);
-    if(e < l || s > r) return 0;
-    if(l <= s && e <= r) return tr[n];
-    ll mid = (s+e)/2;
-    return query(2*n, s, mid, l, r)+query(2*n+1, mid+1, e, l, r);
-}
+            init(1,0,N-1);
+        }
 
-ll init(ll n, ll s, ll e){
-    if(s == e) return tr[n] = arr[s];
-    ll mid = (s+e)/2;
-    return tr[n] = init(n*2, s, mid)+init(n*2+1, mid+1, e);
+        T _update(ll n, ll s, ll e, ll l, ll r, T val){
+            if(lazy[n]) push(n, s, e);
+            if(r < s || l > e) return this->tr[n];
+            if(l <= s && e <= r){
+                lazy[n] = val;
+                push(n,s,e);
+                return this->tr[n];
+            }
+            ll mid = (s+e)/2;
+            return this->tr[n] = func(_update(2*n, s, mid, l, r, val), _update(2*n+1, mid+1, e, l, r, val));
+        }
 
-}
+        T update(ll l, ll r, T val){
+            return _update(1,0,this->N-1, l, r, val);
+        }
+
+        T _query(ll n, ll s, ll e, ll l, ll r){
+            if(lazy[n]) push(n, s, e);
+            if(e < l || r < s) return 0; // this also should be changed from user
+            if(l <= s && e <= r) return tr[n];
+            ll mid = (s+e)/2;
+            return func(_query(2*n, s, mid, l, r), _query(2*n+1, mid+1, e, l, r));
+        }
+
+        T query(ll l, ll r){
+            return _query(1,0,this->N-1, l, r);
+        }
+
+};
+
 
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    ll N, M , K;
-    cin >> N >> M >> K;
-    arr.resize(N);
-    tr.resize(4*N);
-    lazy.resize(4*N);
-    for(int i =0; i<N; i++)
-        cin >> arr[i];
     
-    init(1,0,N-1);
+    ll N, M, K;
+    cin >> N>> M >> K;
+
+    vector<ll> v(N);
+    for(int i =0; i<N; ++i){
+        cin >> v[i];
+    }
     
-    for(int i =0; i<M+K; i++){
-        ll a, b, c;
-        cin >> a >> b >> c;
-        if(a == 1){
-            ll val;
-            cin >> val;
-            update_range(1,0,N-1, b-1,c-1, val);
+    LazySegmentTree<ll> seg(v);
+    
+    ll Q = M+K;
+    while(Q--){
+        ll q, a, b;
+        cin >> q >> a >> b;
+        //cout << q <<" "<<a <<" "<<b<<"\n";
+        if(q == 1) {
+            ll value;
+            cin >> value;
+            seg.update(a-1, b-1, value);
         }
-        else{
-            cout << query(1,0,N-1, b-1,c-1)<<"\n";
-        }
+        else cout << seg.query(a-1,b-1)<<"\n";
     }
 
     return 0;
-}
+} // Lazy SegmentTree
