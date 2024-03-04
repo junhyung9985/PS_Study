42fa150cb11078e2671db82b6e8ab3b734df0aba
Junhyung Kim <48814228+junhyung9985@users.noreply.github.com>
2024-03-04 17:11:41 +0900

    [Gold V] Title: Divisibility Trick, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/30599. Divisibility Trick/Divisibility Trick.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    ll d;
+    cin >> d;
+    for(int i =0; i<d; ++i)
+        cout << d;
+    return 0;
+}/*
+
+1000 * 1000 = 
+
+*/
