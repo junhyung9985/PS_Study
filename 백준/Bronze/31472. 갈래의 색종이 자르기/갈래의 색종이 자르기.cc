3fb75418896d7e126cdfc65e165d55b06212892b
Junhyung Kim <48814228+junhyung9985@users.noreply.github.com>
2024-03-04 13:24:06 +0900

    [Bronze III] Title: 갈래의 색종이 자르기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/31472. 갈래의 색종이 자르기/갈래의 색종이 자르기.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    ll W;
+    cin >> W;
+    cout << 4 * (int)(sqrt(2*W));
+    
+    
+    return 0;
+}
