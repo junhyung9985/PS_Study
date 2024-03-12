2b3e8b74fc8e343f99de8bd12039ef2b866bb15d
Junhyung Kim <48814228+junhyung9985@users.noreply.github.com>
2024-03-13 03:16:54 +0900

    [Platinum IV] Title: 문명, Time: 968 ms, Memory: 103308 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/14868. 문명/문명.cc
+++ 백준/Platinum/14868. 문명/문명.cc
@@ -14,14 +14,14 @@ ll f(ll n){
     return p[n] = f(p[n]);
 }
 
-ll u(ll a, ll b){
+pair<ll,bool> u(ll a, ll b){
     ll pa = f(a), pb = f(b);
-    if(pa == pb) return sz[pa];
+    if(pa == pb) return {sz[pa],false};
     if(sz[pa] < sz[pb]) swap(pa, pb);
     p[pb] = pa;
     sz[pa] += sz[pb];
     sz[pb] = 0;
-    return sz[pa];
+    return {sz[pa],true};
 } 
 
 int main()
@@ -36,50 +36,61 @@ int main()
         cin >> start[i].first >> start[i].second;
     }
 
-    ll l = -1, r = N*N+1;
-    while(l+1 < r){
-        ll mid = (l+r)/2;
 
-        vector<vector<ll>> occupied(N+1, vector<ll>(N+1,0));
-        list<tuple<ll,ll,ll>> q;
+    vector<vector<ll>> occupied(N+1, vector<ll>(N+1,0));
+    vector<vector<ll>> dist(N+1, vector<ll>(N+1,-1));
+    list<pair<ll,ll>> q;
 
-        p.resize(K+1);
-        sz.resize(K+1,1);
+    p.resize(K+1);
+    sz.resize(K+1,1);
+    ll maxx = 0;
+    for(int i =0; i<K; ++i){
+        if(occupied[start[i].first][start[i].second]){
+            maxx = max(maxx, u(occupied[start[i].first][start[i].second], i+1).first);
+        }
+        occupied[start[i].first][start[i].second] = i+1;
+        dist[start[i].first][start[i].second] = 0;
+        q.push_back({start[i].first, start[i].second});
+        p[i+1] = i+1;
+    }    
+    
+    if(maxx == K){
+        cout << 0;
+        return 0;
+    }
 
-        for(int i =0; i<K; ++i){
-            occupied[start[i].first][start[i].second] = i+1;
-            q.push_back({start[i].first, start[i].second, 0});
-            p[i+1] = i+1;
-        }    
-
-        ll maxx = 0;
-        
-        
-        while(!q.empty()){
-            ll y,x,d;
-            tie(y,x,d) = q.front();
-            q.pop_front();
-            for(int k =0; k<4; ++k){
-                ll ny = y + dy[k], nx = x + dx[k];
-                if(ny <= 0 || nx <= 0 || ny > N || nx > N) continue;
-                if(occupied[ny][nx]){
-                    maxx = max(maxx, u(occupied[y][x], occupied[ny][nx]));
+    ll ans = 0;
+    
+    while(!q.empty()){
+        ll y,x;
+        tie(y,x) = q.front();
+        q.pop_front();
+        vector<pair<ll,ll>> to_see;
+        for(int k =0; k<4; ++k){
+            ll ny = y + dy[k], nx = x + dx[k];
+            if(ny <= 0 || nx <= 0 || ny > N || nx > N) continue;
+            if(occupied[ny][nx]){
+                if(dist[ny][nx] > dist[y][x]) continue;
+                ll val;
+                bool flag;
+                tie(val,flag) = u(occupied[y][x], occupied[ny][nx]);
+                //cout << y <<" "<<x <<" "<<ny <<" "<<nx<< " : " << val <<"\n";
+                maxx = max(maxx, val);
+                if(flag) ans = max(ans, max(dist[y][x], dist[ny][nx]));    
+                if(maxx == K){
+                    cout << ans;
+                    return 0;
                 }
-                else if(d < mid){
-                    occupied[ny][nx] = occupied[y][x];
-                    q.push_back({ny,nx,d+1});
-                }
+                
+            }
+            else{
+                occupied[ny][nx] = occupied[y][x];
+                dist[ny][nx] = dist[y][x]+1;
+                q.push_back({ny,nx});
             }
         }
-        if(maxx == K) r = mid;
-        else l = mid;
-
-        p.clear();
-        sz.clear();
-
-    } // O((N^2+KlogK) * logN) = (4000000 * 11 * 11)
-
-    cout << r;
+        
+    }
 
     return 0;
-} // Parametric Search, DSU, BFS, Smaller-to-Larger(?)
+} // DSU, BFS, Smaller-to-Larger(?)
