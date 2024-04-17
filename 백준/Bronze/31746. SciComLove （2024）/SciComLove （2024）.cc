43fc9d97d23d9154a89bee6e395f050bd6d9ccf7
Junhyung Kim <48814228+junhyung9985@users.noreply.github.com>
2024-04-18 04:54:15 +0900

    [Bronze IV] Title: SciComLove (2024), Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/31746. SciComLove （2024）/SciComLove （2024）.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+   ll N;
+   cin >> N;
+   if(N%2) cout <<"evoLmoCicS";
+   else cout <<"SciComLove";
+    return 0;
+    
+}
