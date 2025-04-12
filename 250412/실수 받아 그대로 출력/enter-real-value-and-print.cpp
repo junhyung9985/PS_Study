279501065c65a2a94833c244b9436a25b72c9623
Junhyung Kim <48814228+junhyung9985@users.noreply.github.com>
2025-04-12 22:10:16 +0900

    Title: 실수 받아 그대로 출력, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250412/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    double N;
+    cin >> N;
+    N*=100;
+    N = round(N);
+    cout << N/100;
+    // Please write your code here.
+    return 0;
+}
